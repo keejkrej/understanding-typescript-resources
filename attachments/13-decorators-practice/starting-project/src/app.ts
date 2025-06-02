fe5180cc566e2dd0ad56c13f4e2227b1da2cfcfd
keejkrej <ctyjackcao@outlook.com>
2025-06-02 17:47:27 +0200

    19

--- attachments/13-decorators-practice/starting-project/src/app.ts
+++ attachments/13-decorators-practice/starting-project/src/app.ts
@@ -78,4 +78,62 @@ class ProjectState extends State<Project> {
   }
 }
 
-const projectState = ProjectState.getInstance()
+const projectState = ProjectState.getInstance();
+
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+      validatableInput.minLength != null &&
+      typeof validatableInput.value === 'string'
+  ) {
+    isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+  if (
+      validatableInput.maxLength != null &&
+      typeof validatableInput.value === 'string'
+  ) {
+    isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+  if (
+      validatableInput.min != null &&
+        typeof validatableInput.value === 'number'
+  ) {
+      isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (
+      validatableInput.max != null &&
+          typeof validatableInput.value === 'number'
+  ) {
+      isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+  return isValid;
+}
+
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
+
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+
+}
