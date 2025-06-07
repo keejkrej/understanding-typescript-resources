d63ada6fb873c5f0a3ed85f8f9412462e1fcf83e
keejkrej <ctyjackcao@outlook.com>
2025-06-07 23:00:08 +0200

    19

--- attachments/19-node-ts/app.ts
+++ attachments/19-node-ts/app.ts
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!\n');
+    });
